363a0636eb255951e042e6b981a92928012adf9f
Spyros Desyllas <sdesyllas@hotmail.com>
2015-01-17 17:01:17 +0200

    DoRelax and ProcessBucket first attempt

--- DistributedDeltaStepping/Program.cs
+++ DistributedDeltaStepping/Program.cs
@@ -22,8 +22,9 @@ namespace DistributedDeltaStepping
                 Intracommunicator comm = Communicator.world;
                 int numberOfVerticesPerProcessor = numberOfNodes / comm.Size;
                 int k = 99999; //a very large number, theoritically close to infinite
-
+                //local vertices will have distributed vertices among each processor
                 Vertex[] localVertices = new Vertex[numberOfVerticesPerProcessor];
+                List<Vertex> allVertices = new List<Vertex>();
                 Bucket[] buckets = new Bucket[k];
                 //initialize buckets
                 //for k=1,2,3,4......n set Bk <- 0
@@ -34,26 +35,24 @@ namespace DistributedDeltaStepping
                 if (comm.Rank == 0)
                 {
                     //first create the random graph using .net graph libraries
-                    graph = Utilities.CreateRandomGraph(numberOfNodes);
+                    graph = Utilities.CreateRandomGraph(numberOfNodes, out allVertices);
                 }
-                else // not rank 0
-                {
-                    // program for all other ranks
-                }
+                //broadcast complete graph structure to all processors
+                comm.Broadcast(ref graph, 0);
                 //The vertices are equally distributed among the processors
-                localVertices = comm.ScatterFromFlattened(graph.Select(x=>x.V).ToArray(), numberOfVerticesPerProcessor, 0);
+                localVertices = comm.ScatterFromFlattened(allVertices.ToArray(), numberOfVerticesPerProcessor, 0);
                 //root node distance := 0
                 //all other distances of nodes are set to infinite
                 Utilities.InitVertices(ref localVertices, comm.Rank);
                 if (comm.Rank == 0)
                 {
                     //add root vertice to first bucket, Set B0 <- {rt}
-                    buckets[0].Vertices.Add(graph.First().V);
+                    buckets[0].Vertices.Add(localVertices.First());
                 }
                 //add all other vertices to the last bucket, Set Boo <- V - {rt}
-                buckets[k - 1].Vertices.AddRange(graph.Skip(1).Select(x => x.V));
-                Console.WriteLine("Hello from processor : {0} , I have {1} vertices", comm.Rank, localVertices.Count());
-
+                buckets[k - 1].Vertices.AddRange(localVertices.Skip(1));
+                Console.WriteLine("Hello from processor : {0} , I have {1} vertices", comm.Rank, String.Join(",", localVertices.Select(x=>x.Id)));
+                Console.WriteLine("Processor {0} , have {1} buckets", comm.Rank, buckets.Count());
                 for (int i = 0; i < k; i++) //Epochs
                 {
                     //TODO: ProcessBucket(Buckets[i])
--- DistributedDeltaStepping/Utilities.cs
+++ DistributedDeltaStepping/Utilities.cs
@@ -10,19 +10,27 @@ namespace DistributedDeltaStepping
 {
     public class Utilities
     {
-
         /// <summary>
         /// Create a random graph using random nodes, using Satsuma library for c#
         /// http://satsumagraph.sourceforge.net/doc/html/p_tutorial.html
         /// </summary>
         /// <param name="numberOfNodes">Number of nodes to create</param>
         /// <returns>returns a complete graph with edges and cost for each edge</returns>
-        public static List<DirectEdge> CreateRandomGraph(int numberOfNodes)
+        public static List<DirectEdge> CreateRandomGraph(int numberOfNodes, out List<Vertex> vertices)
         {
             CompleteGraph graph = new CompleteGraph(numberOfNodes, Directedness.Directed); // create a complete graph on 100 nodes
             var cost = new Dictionary<Node, double>(); // create a cost function on the nodes
             int i = 0;
-            foreach (Node node in graph.Nodes()) cost[node] = i++; // assign some integral costs to the nodes
+            vertices = new List<Vertex>();
+            foreach (Node node in graph.Nodes())
+            {
+                cost[node] = i++; // assign some integral costs to the nodes
+                Vertex vertex = new Vertex();
+                vertex.Id = node.Id;
+                vertex.DistanceToRoot = 0;
+                vertices.Add(vertex);
+            }
+            
             Func<Arc, double> arcCost =
                 (arc => cost[graph.U(arc)] + cost[graph.V(arc)]); // a cost of an arc will be the sum of the costs of the two nodes
 
@@ -71,21 +79,56 @@ namespace DistributedDeltaStepping
             }
         }
 
-        public static void Relax(DirectEdge edge, Bucket[] buckets, int delta)
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="u">Source vertex</param>
+        /// <param name="v">Source vertex</param>
+        /// <param name="graphStructure"></param>
+        /// <param name="buckets"></param>
+        /// <param name="delta"></param>
+        public static void Relax(Vertex u, Vertex v, List<DirectEdge> graphStructure, Bucket[] buckets, int delta)
         {
-            var oldBucketIndex = edge.V.DistanceToRoot / delta;
-            if((edge.U.DistanceToRoot + edge.Cost) < edge.V.DistanceToRoot){
+            //get the direct edge corresponding to source and destination
+            var edge = graphStructure.Where(x => x.U.Id == u.Id && x.V.Id == v.Id).FirstOrDefault();
+            
+            var oldBucketIndex = v.DistanceToRoot / delta;
+            if((u.DistanceToRoot + edge.Cost) < v.DistanceToRoot){
                 edge.V.DistanceToRoot = edge.U.DistanceToRoot + edge.Cost;
             }
-            var newBucketIndex = edge.V.DistanceToRoot / delta;
+            var newBucketIndex = v.DistanceToRoot / delta;
             if (newBucketIndex < oldBucketIndex)
             {
-                buckets[(int)oldBucketIndex].Vertices.Remove(edge.V);
-                buckets[(int)newBucketIndex].Vertices.Add(edge.V);
+                buckets[(int)oldBucketIndex].Vertices.Remove(v);
+                buckets[(int)newBucketIndex].Vertices.Add(v);
             }
-            
         }
-        
-        
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="k"></param>
+        /// <param name="graphStructure"></param>
+        /// <param name="buckets"></param>
+        /// <param name="delta"></param>
+        public static void ProcessBucket(int k, List<DirectEdge> graphStructure, Bucket[] buckets, int delta)
+        {
+            var activeBucket = buckets[k];
+            
+            //if its the first iteration all vertices are treated as active vertices
+            var activeVertices = activeBucket.Vertices;
+
+
+            // foreach u E A and for each edge e = {u,v}
+            foreach (Vertex u in activeVertices)
+            {
+                var edges = graphStructure.Where(x => x.U.Id == u.Id);
+                foreach (DirectEdge edge in edges)
+                {
+                    Relax(u, edge.V, graphStructure, buckets, delta);
+                }
+            }
+        }
     }
 }
